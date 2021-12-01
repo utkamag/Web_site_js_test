0179b326ee264dfeaa758a97ac290379515b8e42
Егор Вихарев <92789705+utkamag@users.noreply.github.com>
2021-12-01 17:19:58 +0400

    Add files via upload

--- preloader.js
+++ preloader.js
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
 window.addEventListener("DOMContentLoaded", function (){
     window.onload = function () {
-=======
-window.onload = function () {
->>>>>>> 468e7ff9a25678e9914e042fd4cea529cbdfb017
     let preloader = document.getElementById("preloader")
     preloader.classList.add("hide_preloader")
-    setInterval(function (){
+    setInterval(function () {
         preloader.classList.add("preloader_hidden")
-    },950)
-
-<<<<<<< HEAD
-}
+    })
+    }
 })
-=======
-}
->>>>>>> 468e7ff9a25678e9914e042fd4cea529cbdfb017
