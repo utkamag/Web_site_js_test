468e7ff9a25678e9914e042fd4cea529cbdfb017
Егор Вихарев <92789705+utkamag@users.noreply.github.com>
2021-12-01 16:05:55 +0400

    Новый функционал
    Добавлен функционал сайта: 1) background video 2) preloader

---
+++ preloader.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    let preloader = document.getElementById("preloader")
+    preloader.classList.add("hide_preloader")
+    setInterval(function (){
+        preloader.classList.add("preloader_hidden")
+    },950)
+
+}
